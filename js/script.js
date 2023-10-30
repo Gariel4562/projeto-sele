e1ba8e1da535bae51f5fa8ac21fc09f28834add3
Gabriel <gabriel.mgran20@hotmail.com>
2023-10-29 21:21:02 -0300

    Update
    Atualizando Pag titulos, adicionando fotos dos jogadores.

--- js/script.js
+++ js/script.js
@@ -7,55 +7,19 @@ function pesquisar () {
     img.setAttribute('id', 'foto')
     
     switch(titulos) {
-        case '' :
-            res.innerHTML = `<br><h1></h1><br> <br><br>`
-        img.setAttribute('src', '')
+        case 'Copa do Mundo' :
+            res.innerHTML = `<br><h1>Copa do Mundo</h1></br> <p> Brasil venceu nos anos: 1958, 1962, 1970, 1994, 2002. </p> `
+        img.setAttribute('src', 'imagens/titulos/copa.jpg')
         break;
-        case '' :
-            res.innerHTML = `<br><h1></h1><br> <br><br>`
-        img.setAttribute('src', '')
+        case 'Copa das Confederações' :
+            res.innerHTML = `<br><h1>Copa das Confederações</h1></br> <p> Brasil Venceu nos anos: 1997, 2005, 2009, 2013. </p>`
+        img.setAttribute('src', 'imagens/titulos/confedera.jpg')
         break;
-        case '' :
-            res.innerHTML = `<br><h1></h1><br> <br><br>`
-        img.setAttribute('src', '')
+        case 'Copa América' :
+            res.innerHTML = `</br><h1>Copa América</h1></br> <p> Brasil venceu a Copa América nos anos de: 1919, 1922, 1949, 1989, 1997, 1999, 2004, 2007, 2019.</p>`
+        img.setAttribute('src', 'imagens/titulos/Camerica.jpg')
         break;
-        case '' :
-            res.innerHTML = `<br><h1></h1><br> <br><br>`
-        img.setAttribute('src', '')
-        break;
-        case '' :
-            res.innerHTML = `<br><h1></h1><br> <br><br>`
-        img.setAttribute('src', '')
-        break;
-        case '' :
-            res.innerHTML = `<br><h1></h1><br><br> <br>`
-        img.setAttribute('src', '')
-        break;
-        case 'Copa Mercosul' :
-            res.innerHTML = `<br><h1></h1> <br> <br> <br>`
-        img.setAttribute('src', '')
-        break;
-        case '' :
-            res.innerHTML = `<br><h1></h1> <br> <br> <br>`
-        img.setAttribute('src', '')
-        break;
-        case '' :
-            res.innerHTML = `<br><h1></h1> <br> <br> <br>`
-        img.setAttribute('src', '')
-        break;
-        case '' :
-            res.innerHTML = `<br><h1></h1><br> <br>`
-        img.setAttribute('src', '')
-        break;
-        case '' :
-            res.innerHTML = `<br><h1><br> <br>`
-        img.setAttribute('src', '')
-        break;
-        case '' :
-            res.innerHTML = `<br><h1></h1> <br>  <br> <br>`
-        img.setAttribute('src', '')
-        break;
-
+        
 
     
     
