3214afc55305354848e6b489bc68effd6d21f749
Gabriel <gabriel.mgran20@hotmail.com>
2023-11-30 01:46:44 -0300

    Update
    Ajeitando Texto

--- js/script01.js
+++ js/script01.js
@@ -13,22 +13,22 @@ switch(jogadores) {
         break;
 
     case 'Garrincha' :
-        res.innerHTML = `<h1> Garrincha </h1> <br> <p> Garrincha Conquistou duas Copas do mundo pela Seleção Brasileira 58 e 62, sendo protagonista da copa de 62. Garrincha fez 17 Gols pela seleção, porém possui apenas uma derrota jogando pela seleção em 60 jogos.. foram 52 Vitórias, Sete empates e uma derrota. </p>`
+        res.innerHTML = `<h1> Garrincha </h1> </br> <p> Garrincha Conquistou duas Copas do mundo pela Seleção Brasileira 58 e 62, sendo protagonista da copa de 62. Garrincha fez 17 Gols pela seleção, porém possui apenas uma derrota jogando pela seleção em 60 jogos.. foram 52 Vitórias, Sete empates e uma derrota. </p>`
         img.setAttribute('src', 'imagens/jogadores/garrincha.jpg')
         break;
 
         case 'Ronaldo' :
-            res.innerHTML = `<h1>Ronaldo</h1> <br> <p> Ronaldo Conquistou Duas Copas do Mundo pela Seleção Brasileira, foi protagonista da copa de 2002 e ganhou a Copa de 94 como reserva. Ronaldo ainda ganhou Duas Copas Américas e uma Copa das Confederações. Ronaldo fez 62 gols pela Seleção. </p>   `
+            res.innerHTML = `<h1>Ronaldo</h1> </br> <p> Ronaldo Conquistou Duas Copas do Mundo pela Seleção Brasileira, foi protagonista da copa de 2002 e ganhou a Copa de 94 como reserva. Ronaldo ainda ganhou Duas Copas Américas e uma Copa das Confederações. Ronaldo fez 62 gols pela Seleção. </p>   `
             img.setAttribute('src', 'imagens/jogadores/Ronaldo.jpg')
             break;
 
         case 'Romário' :
-                res.innerHTML = `<h1>Romário</h1> <br> <p> Romário ganhou a Copa de 94 como protagonista da seleção, e ainda conquistou duas Copas Américas e uma Copa das Confederações. Romário fez 55 Gols pela Seleção. </p>  `
+                res.innerHTML = `<h1>Romário</h1> </br> <p> Romário ganhou a Copa de 94 como protagonista da seleção, e ainda conquistou duas Copas Américas e uma Copa das Confederações. Romário fez 55 Gols pela Seleção. </p>  `
                 img.setAttribute('src', 'imagens/jogadores/romário.webp')
                 break;
 
         case 'Rivaldo' :
-           res.innerHTML = `<h1>Rivaldo</h1> <br> <p> Rivaldo conquistou a Copa de 2002 de pela Seleção Brasileira, foi um dos protagonistas ao lado de Ronaldo na conquista. Rivaldo ainda conquistou 1 Copa América e uma uma Copa das confederações pela Seleção Brasileira. Rivaldo fez 37 Gols pela Seleção.         `
+           res.innerHTML = `<h1>Rivaldo</h1> </br> <p> Rivaldo conquistou a Copa de 2002 de pela Seleção Brasileira, foi um dos protagonistas ao lado de Ronaldo na conquista. Rivaldo ainda conquistou 1 Copa América e uma uma Copa das confederações pela Seleção Brasileira. Rivaldo fez 37 Gols pela Seleção.  </p>       `
            img.setAttribute('src', 'imagens/jogadores/rivaldo.jpg')
            break;
                                     
