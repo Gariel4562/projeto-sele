9ec328c5d98115cd5501490bad06e83281c2223c
Gabriel <gabriel.mgran20@hotmail.com>
2023-11-30 01:24:26 -0300

    Update
    Adicionando grandes jogadores que passaram pela seleção, e times históricos.

--- js/script01.js
+++ js/script01.js
@@ -7,81 +7,30 @@ let img = document.createElement('img')
 img.setAttribute('id', 'foto')
 
 switch(jogadores) {
-    case '' :
-    res.innerHTML = `<h1></h1> <br> <br> <br>   `
-        img.setAttribute('src', '')
+    case 'Pelé' :
+    res.innerHTML = `<h1>Pelé</h1> </br> <p> Pelé fez 77 Gols pela Seleção Brasileira, Conquistou três Copas do Mundo pela Seleção Brasileira: 58, 62, 70 (foi protagonista nas copas de 58 e 70.). </p> `
+        img.setAttribute('src', 'imagens/jogadores/PeléSele.webp')
         break;
 
-    case '' :
-        res.innerHTML = ``
-        img.setAttribute('src', '')
+    case 'Garrincha' :
+        res.innerHTML = `<h1> Garrincha </h1> <br> <p> Garrincha Conquistou duas Copas do mundo pela Seleção Brasileira 58 e 62, sendo protagonista da copa de 62. Garrincha fez 17 Gols pela seleção, porém possui apenas uma derrota jogando pela seleção em 60 jogos.. foram 52 Vitórias, Sete empates e uma derrota. </p>`
+        img.setAttribute('src', 'imagens/jogadores/garrincha.jpg')
         break;
 
-        case '' :
-            res.innerHTML = `<h1></h1> `
-            img.setAttribute('src', '')
+        case 'Ronaldo' :
+            res.innerHTML = `<h1>Ronaldo</h1> <br> <p> Ronaldo Conquistou Duas Copas do Mundo pela Seleção Brasileira, foi protagonista da copa de 2002 e ganhou a Copa de 94 como reserva. Ronaldo ainda ganhou Duas Copas Américas e uma Copa das Confederações. Ronaldo fez 62 gols pela Seleção. </p>   `
+            img.setAttribute('src', 'imagens/jogadores/Ronaldo.jpg')
             break;
 
-        case '' :
-                res.innerHTML = `<h1></h1> <br> `
-                img.setAttribute('src', '')
+        case 'Romário' :
+                res.innerHTML = `<h1>Romário</h1> <br> <p> Romário ganhou a Copa de 94 como protagonista da seleção, e ainda conquistou duas Copas Américas e uma Copa das Confederações. Romário fez 55 Gols pela Seleção. </p>  `
+                img.setAttribute('src', 'imagens/jogadores/romário.webp')
                 break;
 
-        case '' :
-           res.innerHTML = `<h1></h1>        
-           `
-           img.setAttribute('src', '')
+        case 'Rivaldo' :
+           res.innerHTML = `<h1>Rivaldo</h1> <br> <p> Rivaldo conquistou a Copa de 2002 de pela Seleção Brasileira, foi um dos protagonistas ao lado de Ronaldo na conquista. Rivaldo ainda conquistou 1 Copa América e uma uma Copa das confederações pela Seleção Brasileira. Rivaldo fez 37 Gols pela Seleção.         `
+           img.setAttribute('src', 'imagens/jogadores/rivaldo.jpg')
            break;
-        
-        case '' :
-            res.innerHTML = `<h1></h1> `
-            img.setAttribute('src', '')
-            break;
-
-            case '' :
-                res.innerHTML = `<h1></h1> `
-                img.setAttribute('src', '')
-                break;
-
-                        case '' :
-                            res.innerHTML = `<h1></h1> `
-                        img.setAttribute('src', '')
-                        break;
-
-                            case '' :
-                                res.innerHTML = `<h1></h1>  `
-                                img.setAttribute('src', '')
-                                break;
-
-                                case '' :
-                                    res.innerHTML = `<h1></h1> <br> `
-                                    img.setAttribute('src', '')
-                                    break;
-
-                                    case '' :
-                                    res.innerHTML = `<h1></h1> `
-                                    img.setAttribute('src', '')
-                                    break;
-
-                                    case '' :
-                                    res.innerHTML = `<h1></h1>  `
-                                    img.setAttribute('src', '')
-                                    break;
-
-                                    case '' :
-                                    res.innerHTML = `<h1></h1>  `
-                                    img.setAttribute('src', '')
-                                    break;
-
-                                      case '' :
-                                      res.innerHTML = `<h1></h1> `
-                                      img.setAttribute('src', '')
-                                      break;
-
-                                      case '' :
-                                      res.innerHTML = `<h1></h1>  `
-                                      img.setAttribute('src', '')
-                                      break;
                                     
                         
     
--- js/script02.js
+++ js/script02.js
@@ -7,21 +7,21 @@ function pesquisar () {
     img.setAttribute('id', 'foto')
     
     switch(times) {
-        case '' :
-            res.innerHTML = "<h1></h1> <br> <p> </p>  " 
-            img.setAttribute('src', '')
+        case 'Seleção58-62' :
+            res.innerHTML = "<h1>Seleção de 58 - 62</h1> <p> Em pé: Vicente Feola (técnico), Djalma Santos, Zito, Bellini, Nilton Santos, Orlando e Gylmar. Agachados: Garrincha, Didi, Pelé, Vavá, Zagallo e Paulo Amaral (Preparador fisico). Grandes Feitos: Foi a única seleção na história a ser Bicampeã do mundo 58 - 62. </p>   " 
+            img.setAttribute('src', 'imagens/times/seleção58-62.webp')
             break;
-        case '' :
-          res.innerHTML = "<h1></p>  "
-          img.setAttribute('src', '')
+        case 'Seleção70' :
+          res.innerHTML = "<h1>Seleção de 70 </h1>   <p>Em Pé: Carlos Alberto Torres, Félix, Piazza, Brito, Clodoaldo, Everaldo e Admildo Chirol (preparador fisico). Agachados: Mária Américo(massagista), Jairzinho, Gérson, Tostão, Pelé, Rivellino e Nocaute Jack (Massagista). Grande Feito: Campeã do Mundo em 1970, Tricampeão do mundo e conquistou a posse definitiva da Taça Jules Rimet ao Brasil. o Técnico da Seleção foi Zagallo.</p>  "
+          img.setAttribute('src', 'imagens/times/seleção70.jpg')
           break;
-        case '' :
-            res.innerHTML = "<h1></h1>  " 
-            img.setAttribute('src', '')
+        case 'Seleção94' :
+            res.innerHTML = "<h1>Seleção de 94</h1> <p> Em pé: Taffarel, Jorginho, Aldair, Mauro Silva, Márcio Santos e Branco. Agachados: Mazinho, Romário, Dunga, Bebeto e Zinho. Grandes feitos: Campeã do Mundo em 1994. Trouxe de volta a Copa depois de 24 anos de jejum. </p>   " 
+            img.setAttribute('src', 'imagens/times/seleção94.webp')
             break;
-            case '' :
-                res.innerHTML = "<h1></h1> " 
-                img.setAttribute('src', '')
+            case 'Seleção2002' :
+                res.innerHTML = "<h1>Seleção de 2002</h1> <p> Em pé: Lúcio, Edmilson, Roque Júnior, Gilberto Silva, Marcos, Kaká, Vampera, Anderson Polga, Dida, Rogério Ceni e Belletti. Agachados: Ronaldinho, Ronaldo, Roberto Carlos, Kleberson, Rivaldo, Cafu, Júnior, Ricardinho, Luizão, Edilson, Denilson e Juninho Paulista. Grandes Feitos: Campeã da Copa do mundo de 2002. Fez do Brasil o primeiro pentacampeão mundial. </p> " 
+                img.setAttribute('src', 'imagens/times/seleção2002.jpg')
                 break;
             
     
