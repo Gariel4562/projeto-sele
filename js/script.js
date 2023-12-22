0bf823cda29dbbfbe49a93efe5b36e5cbb8f5fa5
Gabriel <gabriel.mgran20@hotmail.com>
2023-12-21 23:53:16 -0300

    Update
    Ajuste no titulo.

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function pesquisar () {
             res.innerHTML = `<br><h1>Copa do Mundo</h1></br> <p> Brasil venceu nos anos: 1958, 1962, 1970, 1994, 2002. </p> `
         img.setAttribute('src', 'imagens/titulos/copa.jpg')
         break;
-        case 'Copa das Confederações' :
+        case 'Confederações' :
             res.innerHTML = `<br><h1>Copa Confederações</h1></br> <p> Brasil Venceu nos anos: 1997, 2005, 2009, 2013. </p>`
         img.setAttribute('src', 'imagens/titulos/confedera.jpg')
         break;
